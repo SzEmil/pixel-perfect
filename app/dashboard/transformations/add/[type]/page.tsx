c4796b6480c909269f8ef68479a6348e82c3778b
Emil Szymczyk <106548124+SzEmil@users.noreply.github.com>
2024-02-24 21:13:20 +0100

    feat(Transformations forms): ImageFOrm

--- app/dashboard/transformations/add/[type]/page.tsx
+++ app/dashboard/transformations/add/[type]/page.tsx
@@ -1,10 +1,27 @@
 import { Header } from '@/components/shared/Header/Header';
+import { TransformationForm } from '@/components/shared/TransformationForm/TransformationForm';
 import { transformationTypes } from '@/constants';
+import { getCurrentUser } from '@/lib/actions/user.actions';
+import { auth } from '@clerk/nextjs';
 
-const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+const AddTransformationTypePage = async ({
+  params: { type },
+}: SearchParamProps) => {
   const transformation = transformationTypes[type];
+
+  const user = await getCurrentUser();
   return (
-    <Header title={transformation.title} subtitle={transformation.subTitle} />
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+      <section className="mt-10">
+        <TransformationForm
+          action="Add"
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditbalance}
+        />
+      </section>
+    </>
   );
 };
 
---
+++ components/shared/TransformationForm/TransformationForm.types.ts
@@ -0,0 +1,7 @@
+export enum TransformationFormSchemaNames {
+  title = 'title',
+  aspectRatio = 'aspectRatio',
+  color = 'color',
+  prompt = 'prompt',
+  publicId = 'publicId',
+}
